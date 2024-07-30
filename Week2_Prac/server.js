f1f4021508eb6935ffc48e8b69e29629bb7c9293
Rhys18751996 <18751996@stduent.westernsydney.edu.au>
2024-07-30 00:35:54 +0000

    Adding Week2_Pracs - complete Prac2 work

---
+++ Week2_Prac/server.js
@@ -0,0 +1,104 @@
+console.log("server.js began")
+
+try {
+    var express = require('express')
+} catch (error) {
+    console.log("there was an error importing package url. Run 'npm install express' to install package")
+    console.log("Type 'npm list' to see what packages have been installed.")
+    throw new Error('package express not installed');
+}
+
+try {
+    var url = require('url')
+} catch (error) {
+    console.log("there was an error importing package url. Run 'npm install url' to install package")
+    console.log("Type 'npm list' to see what packages have been installed.")
+    throw new Error('package url not installed');
+}
+
+try {
+    var fs = require('fs')
+} catch (error) {
+    console.log("there was an error importing package fs. Run 'npm install fs' to install package")
+    console.log("Type 'npm list' to see what packages have been installed.")
+    throw new Error('package fs not installed');
+}
+
+
+const port = 3000;
+var app = express();
+
+// Define routes
+// http://localhost:3000/
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// http://localhost:3000/about
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+// http://localhost:3000/demofilePage
+app.get("/demofilePage", (req, res) => {
+  fs.readFile('./demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+});
+
+// http://localhost:3000/contact
+app.get("/contact", (req, res) => {
+  res.send("Contact Page");
+});
+
+// http://localhost:3000/GetWeather
+// http://localhost:3000/GetWeather?year=2024&month=7
+app.get('/GetWeather', (req, res) => 
+{
+  // Extracting query string's or settings as default values
+  var queryString = url.parse(req.url, true).query;
+  var year = queryString.year || new Date().getFullYear();
+  var month = queryString.month || new Date().getMonth()+1;
+
+  // Creating the JSON response to return back as text
+  var weatherData = {
+    year,
+    month,
+    temperature: randomIntFromInterval(10, 35), // Example temperature (you can fetch real data here)
+    condition: getRandomWeatherCondition(), // Example weather condition
+  };
+
+  // return the response code and return the weatherData as json format
+  res.status(200).json(weatherData);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server accessible at http://localhost:${port}`);
+  });
+
+
+  // Extra Functions
+
+  /**
+   * Returns an integer value between the min and max values given.
+   * @param {*} min 
+   * @param {*} max 
+   * @returns integer
+   */
+  function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  /**
+   * This function picks a random weather condition ["Sunny", "Cloudy", "Rainy"] as a string
+   * @returns string
+   */
+  function getRandomWeatherCondition() {
+    const weatherConditions = ["Sunny", "Cloudy", "Rainy"];
+    const randomIndex = Math.floor(Math.random() * weatherConditions.length);
+    return weatherConditions[randomIndex];
+  }
